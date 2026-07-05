03e6bd50e57370b2033913b711443df6e0e961c7
LuLu-ling <LuLu-ling@outlook.com>
2026-07-05 22:54:51 +0800

    辞庐

--- AraonMC/Views/Pages/BrowseView.axaml.cs
+++ AraonMC/Views/Pages/BrowseView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using AraonMC.Core.Domain.Entities;
+using AraonMC.ViewModels.Pages;
 
 namespace AraonMC.Views.Pages;
 
@@ -7,5 +10,20 @@ public partial class BrowseView : UserControl
     public BrowseView()
     {
         InitializeComponent();
+    }
+
+    private void ResourceCard_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (sender is not Border border) return;
+
+        var point = e.GetCurrentPoint(border);
+        if (!point.Properties.IsLeftButtonPressed) return;
+
+        if (border.DataContext is ResourceInfo resource
+            && DataContext is BrowseViewModel vm
+            && vm.DetailCommand.CanExecute(resource))
+        {
+            vm.DetailCommand.Execute(resource);
+        }
     }
 }
