7aaefdcd23c17a38bef2b1e176e373a6fac5ae0e
庐庐绫 <LuLu-ling@outlook.com>
2026-07-08 21:29:37 +0800

    fix: 使用 WindowDrawnDecorations 替换自定义窗口拖动/缩放/按钮实现 (#12)

--- AraonMC/Views/MainWindow.axaml.cs
+++ AraonMC/Views/MainWindow.axaml.cs
@@ -1,6 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
 
 namespace AraonMC.Views;
 
@@ -10,22 +8,4 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-
-    private void TitleBar_PointerPressed(object? sender, PointerPressedEventArgs e)
-    {
-        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-            BeginMoveDrag(e);
-    }
-
-    private void TitleBar_DoubleTapped(object? sender, TappedEventArgs e) => ToggleMaximized();
-
-    private void Minimize_Click(object? sender, RoutedEventArgs e) =>
-        WindowState = WindowState.Minimized;
-
-    private void Maximize_Click(object? sender, RoutedEventArgs e) => ToggleMaximized();
-
-    private void Close_Click(object? sender, RoutedEventArgs e) => Close();
-
-    private void ToggleMaximized() =>
-        WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 }
