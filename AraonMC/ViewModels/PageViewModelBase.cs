5d05f8161030abecacc3dbd0b9b8cc9c1a368220
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-07-24 16:17:20 +0800

    chore(header): add license header

--- AraonMC/ViewModels/PageViewModelBase.cs
+++ AraonMC/ViewModels/PageViewModelBase.cs
@@ -1,3 +1,18 @@
+// Copyright (C) 2026 SALTWOOD and contributors
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 namespace AraonMC.ViewModels;
 
 /// <summary>Base for top-level page view models. Exposes a header title.</summary>
